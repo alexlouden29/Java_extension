d24d5615341161bff4ca9323959e50fea38c1710
Alex Louden <alexlouden29@yahoo.com>
2018-04-15 15:54:21 -0700

    Testing push both assignments

---
+++ Assignment 1/src/Welcome1.java
@@ -0,0 +1,13 @@
+/*
+ * Assignment 1
+ * Alexander Louden
+ * 4/14/2018
+ */
+
+
+public class Welcome1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming!");
+	}
+}
