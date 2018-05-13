09973645100e5fe5aa7e6e06c1d8305df60cccaa
Alex Louden <alexlouden29@yahoo.com>
2018-05-13 16:45:41 -0700

    Completed Assignment 5

--- Assignment 5/src/FindPrimes.java
+++ Assignment 5/src/FindPrimes.java
@@ -3,42 +3,46 @@
  */
 
 /**
- *
- * @author StudentName
+ * @author Alexander Louden
+ * Assignment 5
+ * Exercise 6.25
+ * 5/13/2018
  */
 public class FindPrimes {
        
     public static void main(String[] args) {
         System.out.println("Prime numbers < 10k:");
-        /* Loop 10000 times, checking every number along the way for primeness...
-         * Let the isPrime method do the heavy lifting
-         * If a given number is prime it should be displayed on its own line (non prime numbers are not displayed at all)
-         */
-    }
-    
-    public static boolean isPrime(int num) {
-    	/* There is more than one way to determine if a number is prime or not...
-    	 * We'll take the "easy way out" and verify by brute force that a number is evenly divisible only by 1 and itself:
-         * this method will return true if so (it is prime!) and false otherwise (it is not prime)
-         * You are NOT being asked to implement the Sieve of Eratosthenes algorithm
-         * You MUST use an iterative/loop (no big if-else or switch statements)
-         *
-         * Note: negative numbers aren't prime. isPrime should return false if provided a negative number as its argument
-         */         
         
-        /* Part C has you writing and running the program with upper limits of n/2 and the square root of n.
-         * Only very minimal (as little as a single line) changes are necessary to use one upper limit in place of another.
-         * SUBMIT YOUR SOLUTION USING THE SQUARE ROOT OF N AS THE UPPER LIMIT. 
-         * 
-         * This exercise shows that you can solve the same problem with 
-         * different algorithms... the key takeaway is that, just because one 
-         * approach works doesn't mean it is the best.
-         *
-         * Why are we able to limit our search for even divisors to those numbers less than or equal to the square root?
-         * All of the numbers > the square root must be 
-         * multiplied by a number < the square root to equal the target 
-         * number... so why not just check the numbers <= the square root?
-         */        
+        // Loop 10000 times checking for primes
+        for(int x = 1; x <= 10000; x++) {
+        	if(isPrime(x) == true) {
+        		//Display number if it is prime
+        		System.out.println(x);
+        	}
+        }
     }
     
+    //Method to determine if the given int is prime.  Uses sqrt(n) as upper bound
+    public static boolean isPrime(int num) {
+    	
+    	boolean isPrime = true;
+    	
+    	//Negative numbers, 0 and 1 are not prime
+    	if(num <= 1) {
+    		isPrime = false;
+    	}
+    	
+    	//Loop up to sqrt(n) and check if modulus returns zero
+    	else {
+    		//Floor is used, because if square root is 11.2, there cannot be 2 factors that are both 12 or higher
+    		int limit = (int)Math.floor(Math.sqrt(num));
+    		for(int x = 2; x <= limit; x++) {
+    			if(num%x == 0) {
+    				isPrime = false;
+    			}
+    		}
+    	}
+    	return isPrime;
+    	
+    }      
 }
