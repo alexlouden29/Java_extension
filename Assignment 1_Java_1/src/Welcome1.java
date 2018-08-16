b5ad7279149c337b5ad0ba00fbb62c5bb2068c43
Alex Louden <alexlouden29@yahoo.com>
2018-08-16 16:33:31 -0700

    test

--- Assignment 1_Java_1/src/Welcome1.java
+++ Assignment 1_Java_1/src/Welcome1.java
@@ -2,7 +2,7 @@
  * Assignment 1
  * Alexander Louden
  * 4/14/2018
- */
+ */aa
 
 
 public class Welcome1 {
@@ -10,4 +10,4 @@ public class Welcome1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java Programming!");
 	}
-}
+}aa
